c9c7086d807c7344b75437d8e7496b9684a3a644
Damjan Vukovic <damjan_vukovic@hotmail.com>
2011-09-01 11:38:47 +0200

    third

---
+++ inc/asm.h
@@ -0,0 +1,15 @@
+//File asm.h
+
+#ifndef _ASM_H_
+#define _ASM_H_
+#include <PCB.H>
+#include <LOCK.H>
+
+
+typedef unsigned char IVTNo;
+
+void editAX(PCB* pcb, int value);
+void initISR(IVTNo n, unsigned isrOFF, unsigned isrSEG, unsigned& oldOFF, unsigned& oldSEG, void interrupt(**oldISRptr)());
+void restoreISR(IVTNo n, unsigned oldOFF, unsigned oldSEG);
+
+#endif
---
+++ src/asm.cpp
@@ -0,0 +1,105 @@
+//File: asm.cpp
+
+
+#include <asm.h>
+
+unsigned oldISRSeg, oldISROff;
+unsigned ISRSeg, ISROff;
+
+void interrupt (*oldISR)();
+
+/*ne radi sa dx, zamijenjeno dx svuda sa bx*/
+
+void initISR(IVTNo n, unsigned isrOFF, unsigned isrSEG, unsigned& oldOFF, unsigned& oldSEG, void interrupt (**oldISRptr)()){
+	lock()
+	ISRSeg = isrSEG;
+	ISROff = isrOFF;
+	oldISR=*oldISRptr;
+	asm{
+		push es
+		push ax
+		push bx
+		mov ax, 0
+		mov es, ax
+		mov bx, word ptr n
+		shl bx,1
+		shl bx,1
+		mov ax, word ptr es:bx
+		mov word ptr oldISROff, ax
+		add bx, 2
+		mov ax, word ptr es:bx
+		mov word ptr oldISRSeg, ax
+		mov ax, word ptr ISRSeg
+		mov word ptr es:bx, ax
+		sub bx, 2 
+		mov ax, word ptr ISROff
+		mov word ptr es:bx, ax
+		
+		mov bx, word ptr n
+		add bx, 58h
+		shl bx,1
+		shl bx,1
+		mov ax, oldISROff
+	
+		mov word ptr oldISR[2], es
+		mov word ptr oldISR[0], bx
+		
+		mov word ptr es:bx, ax
+		add bx,2
+		mov ax, oldISRSeg
+		mov word ptr es:bx, ax
+
+		pop bx
+		pop ax
+		pop es
+	}
+	oldOFF = oldISROff;
+	oldSEG = oldISRSeg;
+	unlock()
+}
+void restoreISR(IVTNo n, unsigned oldOFF, unsigned oldSEG){
+	lock()
+	oldISROff = oldOFF;
+	oldISRSeg = oldSEG;
+	asm{
+		push es
+		push ax
+		push bx
+		mov ax, 0
+		mov es, ax
+		mov bx, word ptr n
+		shl bx,1
+		shl bx,1
+		mov ax, oldISROff
+		mov word ptr es:bx, ax
+		add bx, 2
+		mov ax, oldISRSeg
+		mov word ptr es:bx, ax
+		pop bx
+		pop ax
+		pop es
+		
+	}
+	unlock()
+}
+void editAX(PCB* pcb, int value){
+	lock()
+	unsigned temp_sp,temp_ss;
+	if(pcb){
+		temp_sp=pcb->sp;
+		temp_ss=pcb->ss;
+	}else return;
+	asm{
+		push bx
+		push es
+		push dx
+		mov bx, word ptr temp_sp
+		mov es, word ptr temp_ss
+		mov dx, word ptr value
+		mov word ptr es:[bx+16], dx
+		pop dx
+		pop es
+		pop bx
+	}
+	unlock()
+}
