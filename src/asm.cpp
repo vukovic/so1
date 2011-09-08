af6275e9b467967383b6e077a8ebd7408ee8214c
Damjan Vukovic <damjan_vukovic@hotmail.com>
2011-09-08 21:37:32 +0200

    radna

--- src/asm.cpp
+++ src/asm.cpp
@@ -9,8 +9,7 @@ unsigned ISRSeg, ISROff;
 /*ne radi sa dx, zamijenjeno dx svuda sa bx*/
 
 void initISR(IVTNo n, unsigned isrOFF, unsigned isrSEG, unsigned& oldOFF, unsigned& oldSEG, void interrupt (*&oldISRptr)()){
-	asm pushf
-	asm cli
+	lock();
 	ISRSeg = isrSEG;
 	ISROff = isrOFF;
 	asm{
@@ -24,31 +23,19 @@ void initISR(IVTNo n, unsigned isrOFF, unsigned isrSEG, unsigned& oldOFF, unsign
 		shl bx,1
 		mov ax, word ptr es:bx
 		mov word ptr oldISROff, ax
+		mov ax, word ptr ISROff
+		mov word ptr es:bx, ax
 		add bx, 2
 		mov ax, word ptr es:bx
 		mov word ptr oldISRSeg, ax
 		mov ax, word ptr ISRSeg
 		mov word ptr es:bx, ax
-		sub bx, 2 
-		mov ax, word ptr ISROff
-		mov word ptr es:bx, ax
-		
-		mov bx, word ptr n
-		add bx, 58h
-		shl bx,1
-		shl bx,1
-		
-		mov ax, oldISROff
-		mov word ptr es:bx, ax
-		add bx,2
-		mov ax, oldISRSeg
-		mov word ptr es:bx, ax
 		
 		les bx, oldISRptr
 		mov ax, word ptr oldISROff
-		mov word ptr es:[bx], ax
+		mov word ptr es:bx, ax
 		mov ax, word ptr oldISRSeg
-		mov word ptr es:[bx+2], ax
+		mov word ptr es:bx+2, ax
 
 		pop bx
 		pop ax
@@ -56,11 +43,10 @@ void initISR(IVTNo n, unsigned isrOFF, unsigned isrSEG, unsigned& oldOFF, unsign
 	}
 	oldOFF = oldISROff;
 	oldSEG = oldISRSeg;
-	asm popf
+	unlock();
 }
 void restoreISR(IVTNo n, unsigned oldOFF, unsigned oldSEG){
-	asm pushf
-	asm cli
+	lock();
 	oldISROff = oldOFF;
 	oldISRSeg = oldSEG;
 	asm{
@@ -69,19 +55,19 @@ void restoreISR(IVTNo n, unsigned oldOFF, unsigned oldSEG){
 		push bx
 		mov ax, 0
 		mov es, ax
-		mov bx, word ptr n
+		mov bx, ax
+		mov bl, byte ptr n
 		shl bx,1
 		shl bx,1
 		mov ax, oldISROff
 		mov word ptr es:bx, ax
-		add bx, 2
 		mov ax, oldISRSeg
-		mov word ptr es:bx, ax
+		mov word ptr es:bx + 2, ax
 		pop bx
 		pop ax
 		pop es
-		popf
 	}
+	unlock();
 }
 void editAX(PCB* pcb, int value){
 	asm pushf
