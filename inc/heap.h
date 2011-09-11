7a1f0d6f563b32f19e652f848e6301302d3944de
Damjan Vukovic <damjan_vukovic@hotmail.com>
2011-09-11 21:50:15 +0200

    radna

---
+++ inc/heap.h
@@ -0,0 +1,56 @@
+//File: heap.h
+
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+#include <KrnlSem.h>
+#include <PCB.H>
+
+class Heap{
+public:
+	Heap(KernelSem *ks):myKernelSem(ks),last(-1),capacity(30), timeNow(0){
+		lock();
+		heap = new Elem[capacity];
+		unlock();
+	}
+	~Heap(){delete [] heap;}
+	
+	void put (PCB* pcb, int prio);
+	PCB* get();
+	void remove (PCB* pcb);
+	
+	PCB* getHeadPCB(){return (last>=1)?heap[1].pcb:0;}
+	int incTime(){return ++timeNow && last>=1 && heap[1].prio && ((timeNow - heap[1].time0)>=heap[1].prio);}
+protected:
+	class Elem{
+	public:
+		Elem(PCB* _pcb, int prio, unsigned long int _time0, Heap* _myHeap):pcb(_pcb),time0(_time0),myHeap(_myHeap){}
+		friend int operator<(Elem& e1, Elem& e2);
+		friend int operator>(Elem& e1, Elem& e2);
+		friend int operator == (Elem& e1, Elem& e2);
+		int priority();
+	private:
+		Heap* myHeap;
+		PCB* pcb;
+		int prio;
+		unsigned long int time0;
+	};
+private:
+	void remove (int indx = 1);
+	void wake(int indx, int ax);
+	
+	void propellUp(int indx);
+	void propellDown(int indx);
+	
+	int lChild(int indx){return 2*indx;}
+	int rChild(int indx){return 2*indx+1;}
+	int parent(int indx){return indx/2;}
+	
+	KernelSem *myKernelSem;
+	Elem *heap;
+	int last;
+	int capacity;
+	unsigned long int timeNow;
+};
+
+#endif
